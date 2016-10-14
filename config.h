2c14e64bde2c9371f62f15466d6a9131b0d8800b
Mgarrigo <mariano.garrigo@gmail.com>
2016-10-14 21:28:24 +0200

    Add files via upload
    Config comments.

--- config.h
+++ config.h
@@ -1,9 +1,19 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+/* Server Configuration file */
+
+/* Maximum amount of players allowed on BlackJack Table (Int > 1) */
 #define PLAYERS 4
+
+/* Starting Balance for new players (Int > 1) */
 #define STARTING_BALANCE 100
+
+/* Amount of decks (BlackJack uses many 52-Cards decks to play) (Int > 1) */
 #define PLAYING_DECKS 6
+
+/* How far the deck will go until it is shuffled (Between 0.1 and 0.8)
+(E. 0.7 means when deck is 70% used, it shuffles) */
 #define DECK_PENETRATION 0.7
 
 #endif
