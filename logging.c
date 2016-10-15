f5311a108499e8da56824de86eaa99fb9b0a4307
matuteale <mbuscagl@itba.edu.ar>
2016-10-15 19:43:11 -0300

    Now the logging writes on a log.txt file on /tmp/ path

--- logging.c
+++ logging.c
@@ -1,6 +1,7 @@
 //Basado en las instrucciones de https://users.cs.cf.ac.uk/Dave.Marshall/C/node25.html
 
 #include "logging.h"
+#include <stdio.h>
 
 int main() {
     int msgQueueId;
@@ -9,25 +10,35 @@ int main() {
         perror("msgget");
         exit(1);
     }
+    FILE * fp;
 
-    while(1){
+    fp = fopen("/tmp/log.txt", "w+" );
 
-        if (msgrcv(msgQueueId, &rbuf, MSG_SIZE, 0, 0) < 0) {
-            perror("msgrcv");
-            exit(1);
-        }
+    while(msgrcv(msgQueueId, &rbuf, MSG_SIZE, 0, 0) >= 0){
+
+        fp = fopen("/tmp/log.txt", "a+" );
 
         if(rbuf.mtype == 1){ // Info
             printf("[Info]: %s\n", rbuf.mtext);
+            fputs("[Info]: ", fp);
+            fputs(rbuf.mtext, fp);
+            fputs("\n", fp);
         }
         if(rbuf.mtype == 2){ // Warning
             printf("[Warning]: %s\n", rbuf.mtext);
+            fputs("[Warning]: ", fp);
+            fputs(rbuf.mtext, fp);
+            fputs("\n", fp);
         }
         if(rbuf.mtype == 3){ // Error
             printf("[Error]: %s\n", rbuf.mtext);
+            fputs("[Error]: ", fp);
+            fputs(rbuf.mtext, fp);
+            fputs("\n", fp);
         }
-
-
+        fclose(fp);
     }
+    perror("msgrcv");
+    fclose(fp);
 }
 
